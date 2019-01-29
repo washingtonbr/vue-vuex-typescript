61affe2736053d12f6fe8cdef1bdf49c49c9891d
Washington Pires <4washingtonbr@gmail.com>
2019-01-29 00:48:44 -0200

    Declaração do componente "Country" no componente "App".

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Country/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Country from './components/Country.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Country,
   },
 })
 export default class App extends Vue {}
