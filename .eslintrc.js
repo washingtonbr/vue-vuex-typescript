64d386ef7b836525499d9c1e242eb228098f240f
Washington Pires <4washingtonbr@gmail.com>
2019-01-29 00:47:52 -0200

    Configuração no arquivo ".eslintrc.js".

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     '@vue/typescript',
   ],
   rules: {
+    'ignorePropertyModificationsFor': ["error", { "props": false }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
